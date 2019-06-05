b3ad3ce963a63cd2e170e97276f094fbd6ee7916
junipera <juniper.alcorn@gmail.com>
2019-06-05 15:16:50 -0400

    install sequelize

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser')
+
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.use(bodyParser.json())
+// app.use(
+//   bodyParser.urlencoded({
+//     extended: true,
+//   })
+// )
 
 app.listen(PORT, () => {
   console.log(`Express server listening on port ${PORT}`);
