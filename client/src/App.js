ecf8352cb0ed14039ed8f06802e6ae2d90506e58
junipera <juniper.alcorn@gmail.com>
2019-06-08 09:29:04 -0400

    portfolio

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 //components
 import Header from './components/Header'
 import About from './components/About'
+import Portfolio from './components/Portfolio'
 
 function App() {
   return (
     <div className="App">
       <Header />
       <About />
+      <Portfolio />
 
     </div>
   );
---
+++ client/src/components/Portfolio.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+
+class Portfolio extends Component{
+    constructor(props){
+        super(props)
+        this.state={}
+    }
+    render(){
+        return(
+            <div className='portfolio'>
+                Portfolio section
+            </div>
+        )
+    }
+}
+export default Portfolio
